114412149b8cbaf5f6f921a095f5e0b460371b24
AlexPerrot <alexandre.perrot@gmail.com>
2012-12-17 11:35:21 +0100

    un peu d'ordre

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -1,5 +1,8 @@
 #include "graph.h"
 
+
+/****** VertexProperties Implementation ******/
+
 VertexProperties::VertexProperties() : vertexColor(-1) {
 	
 }
@@ -11,6 +14,9 @@ color VertexProperties::getColor() {
 void VertexProperties::setColor(color newColor) {
 	vertexColor = newColor;
 }
+
+
+/****** Graph Implementation ******/
 
 Graph::Graph() : boostDeleg() {
 	
@@ -39,6 +45,9 @@ VertexIterator Graph::getNeighbours(vertex v) {
 	//utiliser adjacent_vertices, mais il renvoit des adjacency_iterator
 	//return VertexIterator(0,0);
 }
+
+
+/****** VertexIterator Implementation ******/
 
 VertexIterator::VertexIterator(vertexIterator begin, vertexIterator end)
  : current(begin), end(end) {
